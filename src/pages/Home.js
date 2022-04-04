a0d6e7defdc4662f295a786efa9f7f4a01b66de8
Victoria Le <victoriale@Victorias-MacBook-Pro.local>
2022-04-03 20:01:29 -0400

    adding useDispatch

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ import Message from '../components/Message'
 import {listBooks} from '../actions/bookActions'
 
 import {useEffect} from 'react'
-import {useSelector} from 'react-redux'
+import {useSelector,useDispatch} from 'react-redux'
 import {useNavigate} from 'react-router-dom'
 
 const Home = () => {
